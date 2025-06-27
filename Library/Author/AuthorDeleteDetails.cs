cf615262f915e59e263005f28be47c6b92f206f8
Jaya Krishna <144379958+jaykrishna77@users.noreply.github.com>
2025-06-27 17:23:32 +0530

    Add files via upload

---
+++ Library/Author/AuthorDeleteDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Library.UserLogin;
+
+namespace Library
+{
+    public partial class AuthorDeleteDetails : Form
+    {
+        public AuthorDeleteDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void lstbxDeleteDetails_Click(object sender, EventArgs e)
+        {
+            lstbxDeleteDetails.Items.Add("1");
+            lstbxDeleteDetails.Items.Add("2");
+            lstbxDeleteDetails.Items.Add("3");
+            lstbxDeleteDetails.Items.Add("4");
+            lstbxDeleteDetails.Items.Add("5");
+            lstbxDeleteDetails.Items.Add("6");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            clsUserLogin author = new clsUserLogin();
+            //string strId = txtbxauthor_id.Text;
+            int iId = Convert.ToInt32(lstbxDeleteDetails.Text);
+            author.DeleteAuthorQuery(iId);
+            this.Close();
+        }
+    }
+}
---
+++ Library/Author/AuthorUpdateDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Library.UserLogin;
+
+namespace Library.Author
+{
+    public partial class AuthorUpdateDetails : Form
+    {
+        public AuthorUpdateDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            clsUserLogin author = new clsUserLogin();
+            int iId = Convert.ToInt32(txtbxauthor_id.Text);
+            string strName = txtbxAuthorName.Text;
+            string strStatus = lstbxAuthorStatus.Text;
+            author.UpdateAuthorQuery(iId,strName, strStatus);
+            this.Close();
+        }
+    }
+}
